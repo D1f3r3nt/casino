3f81048d97fb0204d98c063a14adcb2ae5c09883
D1f3r3nt <98466831+D1f3r3nt@users.noreply.github.com>
2023-02-06 16:35:28 +0100

    Subido blackjack

--- blackjack/juagador.py
+++ blackjack/juagador.py
@@ -2,25 +2,41 @@ class Jugador:
     cartas = []
     puntos = 0
     alive = True
+    nombre = ""
+    _restas = 0
 
-    def __init__(self):
+    def __init__(self, nombre):
+        self.cartas = []
+        self.puntos = 0
+        self.alive = True
+        self.nombre = nombre
+        self._restas = 0
         pass
 
     def cogerCarta(self, carta):
         self.cartas.append(carta)
         valor = carta[:-1]
+
         if (valor == 'J' or valor == 'Q' or valor == 'K'):
             valor = 10
         elif (valor == 'A'):
-            valor = 1
-            # Modificar valor propero
+            valor = 11
+            self._restas += 1
 
         self.puntos += int(valor)
+
+        while self.puntos > 21 and self._restas > 0:
+            self.puntos -= 10
+            self._restas -= 1
 
         if (self.puntos > 21):
             self.alive = False
 
     def mostrar(self):
-        print('Jugador(' + str(self.puntos) + 'p): ')
+        print(self.nombre + ' (' + str(self.puntos) + 'p) : ')
         for carta in self.cartas:
             print('==> ' + carta)
+
+    def mostarSoloPrimera(self):
+        print(self.nombre + ' : ')
+        print('==> ' + self.cartas[0])
--- blackjack/mesa.py
+++ blackjack/mesa.py
@@ -1,21 +1,22 @@
+import time
 from blackjack import carta, juagador
 class Mesa:
     baraja = carta.Carta()
-    player = juagador.Jugador()
-    crupiere = juagador.Jugador()
+    player = juagador.Jugador('Jugador')
+    crupiere = juagador.Jugador('Crupiere')
 
     def __init__(self):
         self._crupiereInit()
         self._playerInit()
         self._playerJuego()
         self._crupiereJuego()
-        print('Fin de la partida')
+        self._finPartida()
         pass
 
     def _crupiereInit(self):
         self.crupiere.cogerCarta(self.baraja.cogerUna())
         self.crupiere.cogerCarta(self.baraja.cogerUna())
-        self.mostrar()
+        self.crupiere.mostarSoloPrimera()
 
     def _playerInit(self):
         self.player.cogerCarta(self.baraja.cogerUna())
@@ -33,18 +34,38 @@ class Mesa:
             response = input()
 
             self.player.cogerCarta(self.baraja.cogerUna())
-            self.mostrar()
+            self.crupiere.mostarSoloPrimera()
             self.player.mostrar()
+        time.sleep(2)
 
     def _crupiereJuego(self):
-        while self.player.alive and self.crupiere.alive:
+        while self.player.alive and self.crupiere.alive and self.crupiere.puntos < 17:
             print('Juega el crupier, ver juego')
             input()
 
-            self.player.cogerCarta(self.baraja.cogerUna())
-            self.mostrar()
+            self.crupiere.cogerCarta(self.baraja.cogerUna())
+            self.crupiere.mostrar()
             self.player.mostrar()
+        time.sleep(2)
 
-
-    def mostrar(self):
-        print('Crupiere: ' + self.crupiere.cartas[0])
+    def _finPartida(self):
+        print()
+        print('==========================================')
+        print('==========================================')
+        print()
+        self.crupiere.mostrar()
+        self.player.mostrar()
+        print()
+        print('==========================================')
+        print()
+        print('Fin de la partida')
+        print()
+        if self.player.alive == False or self.crupiere.puntos > self.player.puntos:
+            print('Gana la casa')
+        elif self.crupiere.alive == False or self.crupiere.puntos < self.player.puntos:
+            print('Gana el jugador')
+        else:
+            print('EMPATE')
+        print()
+        print('==========================================')
+        print()
