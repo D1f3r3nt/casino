0e04328e812d74692182924a485388e54a6908c5
D1f3r3nt <98466831+D1f3r3nt@users.noreply.github.com>
2023-02-10 20:11:34 +0100

    arreglado

--- ruleta/ruleta.py
+++ ruleta/ruleta.py
@@ -2,8 +2,11 @@ import random
 class Ruleta:
     def __init__(self):
         self.numeros = [0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26]
-        self.colores = ['V', 'R', 'N', 'R', 'N', 'R', 'N', 'R', 'N', 'R', 'N', 'N', 'R', 'N', 'R', 'N', 'R', 'N', 'R', 'R', 'N', 'R', 'N', 'R', 'N', 'R', 'N', 'N', 'R', 'N', 'R', 'N', 'R']
-        
+        self.colores = ['V', 'R', 'N', 'R', 'N', 'R', 'N', 'R', 'N', 'R', 'N', 'N', 'R', 'N', 'R', 'N', 'R', 'N', 'R', 'R', 'N', 'R', 'N', 'R', 'N', 'R', 'N', 'N', 'R', 'N', 'R', 'N', 'R', 'R', 'N', 'R', 'N']
+
+        print(len(self.numeros))
+        print(len(self.numeros))
+
         # a <= N <= b
         index = random.randint(0, 36)
 
